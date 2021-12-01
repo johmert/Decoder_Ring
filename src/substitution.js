999881dba0f4cb5b0cd69eaf769f1de3a9395fe6
johmert <jessica@ohmert.com>
2021-11-30 16:12:03 -0800

    checkAlphabet helper function

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,24 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  const standardAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  function checkAlphabet(alphabet){
+    if(alphabet.length !== 25) return false;
+    for(let i=0; i<alphabet.length; i++){
+      for(let j=1; j<alphabet.length; j++){
+        if(alphabet[i] === alphabet[j]){
+          return false;
+        }
+      }      
+    }
+    return true;
+  }
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if(!checkAlphabet(alphabet)) return false;
+    let message = input.toLowerCase();
+    
   }
 
   return {
